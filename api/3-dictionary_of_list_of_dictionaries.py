136d70e0c4d89c08cdafdf800ea04b17a664429c
mathpvx <7547@holbertonstudents.com>
2024-04-19 17:41:10 +0200

    refactored the code

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -3,32 +3,46 @@
 list progress identified by ID, using REST API.
 Export data in Json format."""
 
-
-import requests
 import json
+import requests
+
+
+def rest_api():
+    url_users = 'https://jsonplaceholder.typicode.com/users'
+
+    response = requests.get(url_users)
+    if (response.ok):
+        users = json.loads(response.content)
+    else:
+        response.raise_for_status()
+
+    all_tasks = {}
+
+    for user in users:
+        user_id = str(user["id"])
+        EMPLOYEE_NAME = user["username"]
+
+        url_todos = "https://jsonplaceholder.typicode.com/todos"
+        query = {'userId': user_id}
+
+        response = requests.get(url_todos, params=query)
+        if (response.ok):
+            jData = json.loads(response.content)
+
+            tasks = []
+            for task in jData:
+                tasks.append({
+                    "username": EMPLOYEE_NAME, "task": task.get("title"),
+                    "completed": task.get("completed")
+                })
+
+            all_tasks[user_id] = tasks
+        else:
+            response.raise_for_status()
+
+    with open("todo_all_employees.json", "w") as json_file:
+        json.dump(all_tasks, json_file)
 
 
 if __name__ == "__main__":
-    todo_json = requests.get("https://jsonplaceholder.typicode.com/todos")
-    emps_info_json = requests.get("https://jsonplaceholder.typicode.com/users")
-
-    """converts responses to lists"""
-    todo = todo_json.json()
-    emps_info = emps_info_json.json()
-
-    json_obj = {}
-    tasks_list = []
-
-    for emp_info in emps_info:
-        for task in todo:
-            if emp_info.get("id") == task.get("userId"):
-                task_dict = {}
-                task_dict["task"] = task.get("title")
-                task_dict["completed"] = task.get("completed")
-                task_dict["username"] = emp_info.get("username")
-                tasks_list.append(task_dict)
-        json_obj[str(emp_info.get("id"))] = tasks_list
-
-    filename = "todo_all_employees.json"
-    with open(filename, 'w') as file:
-        json.dump(json_obj, file)
+    rest_api()
